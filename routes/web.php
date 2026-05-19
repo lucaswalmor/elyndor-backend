f6a65dc80633d9a8828f3502be88d4ac2755df4b
Lucas Steinbach <lucaswsb52@gmail.com>
2026-05-19 13:12:59 -0300

    feat: add route for accessing game balance log file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/log-temp-debug', function () {
+    return response()->file(storage_path('logs/game-balance-2026-05-19.log'));
+});
